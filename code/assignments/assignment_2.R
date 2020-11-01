d9ef577eb7f3d1db9fb0e41ae7b53eeb47812efb
Jusup  Matej <mjusup@ethz.ch>
2020-11-01 19:46:53 +0100

    Added code to remove outliers and high-leverage points

--- code/assignments/assignment_2.R
+++ code/assignments/assignment_2.R
@@ -4,12 +4,10 @@
 
 # 00 set up ----
 
-# install packages
-install.packages(tidyverse)
-install.packages(lubridate)
-install.packages(here)
-install.packages(AER)
-install.packages("stargazer")
+pkgs <- c("tidyverse", "lubridate", "broom", "dplyr", "here", "purrr", "AER", "stargazer", "leaps")
+new_packages <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
 
 # load packages
 library(tidyverse) # see cheatsheet for dplyr and ggplot2
@@ -17,57 +15,97 @@ library(lubridate) # see cheatsheet for lubridate
 library(here)
 library(AER) # used for regression
 library(stargazer) # nice latex summary output
-
-# Clear workspace aka environment: be careful with this ;-)
-rm(list = ls())
+library(leaps) # computing stepwise regression
+library(MASS)
 
 # see where your path goes
-here::here()
+here()
 
-# load data if necessary
-# load(here::here("data","Mobidrive_2002.RData"))
-
-# Check function here::here() to see how you can define the path to the file location
-# e.g. load(here::here("data","Mobidrive_2002.RData")) if you have a folder called "data" in your project directory
-
-
+rm(list=ls())
 # 0 create variables ----
+var_names <- c("MN_O_PT", "SEX", "AGE", "HH_H", "PARENT", "CHILD", "MARRIED", "EDUC", "EMPLOYED", "WORKING", "N_O_J", "N_O_WP",
+          "N_O_WH", "STUDENT", "LIC_VEH", "LIC_MC", "M_CAR_U", "N_O_HHM", "N_O_EM", "N_O_D", "N_O_PV", "N_O_CYC", "N_O_MC",
+          "N_O_MP", "N_O_V", "N_O_MV", "HH_INC", "N_O_VEH")
 
+train_df <- p_hh %>% 
+            dplyr::select(var_names) %>% 
+            mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .))
+summary(train_df)
+ggplot(gather(train_df), aes(x=value)) + 
+  geom_histogram(bins=10) + 
+  facet_wrap(~key, scales = 'free_x')
 
+train_df$AGE_G <- cut(train_df$AGE, 
+                    breaks=c(0, 18, 30, 55, 70, 100), 
+                    include.lowest=TRUE, 
+                    right=TRUE, 
+                    labels=c("young", "adult", "middle-age", "pension", "old"))
 
+train_df$HH_INC_LN <- log(train_df$HH_INC)
+train_df$HH_INC_EXP <- exp(train_df$HH_INC)
+train_df <- train_df %>%
+            mutate(EDUC_F = replace(EDUC, EDUC %in% 0:4, "low")) %>%
+            mutate(EDUC_F = replace(EDUC_F, EDUC_F %in% 5:8, "middle")) %>%
+            mutate(EDUC_F = replace(EDUC_F, EDUC_F %in% 9:11, "high"))
+
+vars_factor <- c("SEX", "AGE_G", "HH_H", "PARENT", "CHILD", "EDUC_F", "MARRIED", "STUDENT",
+                 "LIC_VEH", "LIC_MC", "M_CAR_U" ) #"EMPLOYED", "WORKING"
+vars_numeric <- c("HH_INC", "N_O_WP", "N_O_WH", "N_O_HHM", "N_O_EM",
+                  "N_O_D", "N_O_CYC", "N_O_MC", "N_O_MP", "N_O_VEH") #, "N_O_J", "N_O_PV",  "N_O_V", "N_O_MV", "HH_INC_LN", "HH_INC_EXP"
+target <- c("MN_O_PT")
+
+train_df[,vars_factor] <- lapply(train_df[,vars_factor], factor)
+train_df <- train_df %>% dplyr::select(c(all_of(target), vars_factor, vars_numeric)) 
 
 # 1 fit a model ----
-model1 <- lm(MN_O_PT ~ AGE + factor(EDUC), data = p_hh)
-
-model2 <- lm(MN_O_PT ~ AGE + log(HH_INC), data = p_hh)
+# Fit the full model
+full.model <- lm(formula=MN_O_PT ~ ., data=train_df)
+# Stepwise regression model
+step.model <- stepAIC(full.model, direction = "both", trace = FALSE)
 
 
 #basic stuff
-summary(model1) # get a summary output
-summary(model2)
+summary(full.model)
+summary(step.model) # get a summary output
 
-model1$na.action # see if there were NA values -> missingness
-names(model1) # see what you can access in the model
-coef(model1) # extract the coefficients
-confint(model1) # get the confidence interval for your parameters
+
+step.model$na.action # see if there were NA values -> missingness
+names(step.model) # see what you can access in the model
+coef(step.model) # extract the coefficients
+confint(step.model) # get the confidence interval for your parameters
 
 
 # 2 test assumptions ----
 # basic infos, but many insights
 par(mfrow=c(2,2)) # Change the panel layout to 2 x 2
-plot(model1)
+plot(step.model)
 par(mfrow=c(1,1)) # Change back to 1 x 1
 
 
 # heteroskedasticity: what is H0 and what is HA?
-bptest(model1)
+bptest(step.model)
 
+
+# Studentized residuals
+plot(step.model$fitted.value, rstudent(step.model), main="Studentized residuals vs. Fitted", xlab="Fitted values", ylab="Studentized residuals")
+abline(h=0, lty=2)
+# We want to get rid of observations where studentized residuals have an absolute value higher than 3
+stud <- abs(rstudent(step.model)) > 3
+sum(stud)
+train_df <- train_df[!stud, ] # refit the model
+# After two iterations we are good
+
+# We want to discard high-leverage points as well
+lev <- cooks.distance(step.model) > (4/nrow(train_df))
+sum(lev)
+nrow(train_df)
+train_df <- train_df[!lev, ]
 
 # (multi-) collinearity
-vif(model1)
+vif(step.model)
 
 # latex output
-stargazer(model1) # copy output directly to overleaf
+stargazer(step.model) # copy output directly to overleaf
 
 
 ######################################################################################
